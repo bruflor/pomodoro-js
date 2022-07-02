4d168b55d4bb676b833ade5fdca3db57e8551ab3
Bruna Flôr <brn.flor@gmail.com>
2022-07-02 17:18:57 +0100

    Play button as stop button

--- src/components/Clock/Timer/Timer.tsx
+++ src/components/Clock/Timer/Timer.tsx
@@ -6,51 +6,52 @@ export const MyTimer = () => {
   const [displayTime, setDisplayTime] = useState(0);
   const [sessionStatus, setSessionStatus] = useState("working");
   const [sessions, setSessions] = useState(0);
-
+  const [timerOn, setTimerOn] = useState(false);
   const sessionNumber = 3;
   const focusTime = 25;
   const shortBreakTime = 5;
   const LongBreakTime = 60;
 
-  const timeSetTime = 60 * 1000; //one minute;
-
+  const timeSetTime = 0.1 * 1000; //one minute;
   useEffect(() => {
-    if (sessionStatus === "working" && sessions < sessionNumber) {
-      if (displayTime < focusTime) {
-        console.log(displayTime);
-        setTimeout(() => {
-          setDisplayTime(displayTime + 1);
-        }, timeSetTime);
-      } else {
-        setSessionStatus("short break");
-        console.log("Start break");
-        setDisplayTime(0);
+    if (!timerOn) {
+      if (sessionStatus === "working" && sessions < sessionNumber) {
+        if (displayTime < focusTime) {
+          console.log(displayTime);
+          setTimeout(() => {
+            setDisplayTime(displayTime + 1);
+          }, timeSetTime);
+        } else {
+          setSessionStatus("short break");
+          console.log("Start break");
+          setDisplayTime(0);
+        }
       }
-    }
-    //short break if
-    else if (sessionStatus === "short break" && sessions < sessionNumber) {
-      if (displayTime < shortBreakTime) {
-        console.log(displayTime);
-        setTimeout(() => {
-          setDisplayTime(displayTime + 1);
-        }, timeSetTime);
-      } else {
-        setSessionStatus("working");
-        console.log("Start session");
-        setSessions(sessions + 1);
-        setDisplayTime(0);
+      //short break if
+      else if (sessionStatus === "short break" && sessions < sessionNumber) {
+        if (displayTime < shortBreakTime) {
+          console.log(displayTime);
+          setTimeout(() => {
+            setDisplayTime(displayTime + 1);
+          }, timeSetTime);
+        } else {
+          setSessionStatus("working");
+          console.log("Start session");
+          setSessions(sessions + 1);
+          setDisplayTime(0);
+        }
       }
-    }
-    //Long break if
-    else {
-      if (displayTime < LongBreakTime) {
-        setSessionStatus("Long Break");
-        console.log(displayTime);
-        console.log("Start Long Break");
-        setTimeout(() => {
-          setDisplayTime(displayTime + 1);
-        }, timeSetTime);
-      } else {
+      //Long break if
+      else {
+        if (displayTime < LongBreakTime) {
+          setSessionStatus("Long Break");
+          console.log(displayTime);
+          console.log("Start Long Break");
+          setTimeout(() => {
+            setDisplayTime(displayTime + 1);
+          }, timeSetTime);
+        } else {
+        }
       }
     }
   }, [displayTime]);
@@ -77,10 +78,13 @@ export const MyTimer = () => {
         </>
       )}
 
-      {/* <button onClick={controlTime}>
-        play
-        {timeOn ? <Pause size={32} /> : <Play size={32} />}
-      </button> */}
+      <button
+        onClick={() => {
+          setTimerOn(!timerOn);
+        }}
+      >
+        {timerOn ? <Pause size={32} /> : <Play size={32} />}
+      </button>
       {/* <button onClick={resetTime}>
         <Repeat size={32} />
       </button> */}
